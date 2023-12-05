f01afb6f69e7d04a0ab287226289581d6b85c63e
eunmdb <32080056+eunmdb@users.noreply.github.com>
2023-12-04 18:33:01 -0800

    added constants

---
+++ kswap/Constants.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct Constants{
+    static var supabaseURL: String { "https://uynrfdulqtssevwuiqrw.supabase.co" }
+    static var supabaseKey: String { "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5bnJmZHVscXRzc2V2d3VpcXJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEyOTIzNjksImV4cCI6MjAxNjg2ODM2OX0.KC6YJ35Yq7xrs1TKiPqnSujeW8f7cF6PchiAXaLzGJE"
+    }
+    
+}
+
+
+
